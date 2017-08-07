2ebaca8d378a5853a88afff2b87fff7e4ea8ef31
Chetan <Chetan@Chetan-PC.fios-router.home>
2017-08-06 22:48:59 -0400

    Updated first change

--- shoppingparent/frontend/src/main/frontend/src/app/app.component.ts
+++ shoppingparent/frontend/src/main/frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Coding Starts !!';
+  debugger;
+  constructor(private router: Router) { }
+  	loginClick= function () {
+  		this.title = 'Login Starts';
+        this.router.navigate(['./login']);
+	};
 }
--- shoppingparent/frontend/src/main/frontend/src/app/app.module.ts
+++ shoppingparent/frontend/src/main/frontend/src/app/app.module.ts
@@ -2,8 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginModule } from './../login/login.module';
+import { LoginComponent } from './../login/login.component';
 
 import { AppComponent } from './app.component';
+
+const appRoutes: Routes = [
+    {path: 'login', component: LoginComponent}
+];
 
 @NgModule({
   declarations: [
@@ -12,7 +19,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    LoginModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
